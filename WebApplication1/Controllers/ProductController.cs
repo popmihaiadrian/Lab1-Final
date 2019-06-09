aaac6e9fae2ca569ff6fe383b2d5c6d0df4d16c3
Mihai Adrian Pop <popmihaiadrian@gmail.com>
2019-06-09 11:05:53 +0200

    Final

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private ProductDbContext context;
+
+        public ProductDbContext Context { get => context; set => context = value; }
+
+        public ProductController(ProductDbContext context)
+        {
+            this.Context = context;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return Context.Products;
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            var existing = Context.Products.FirstOrDefault(product => product.Id == id);
+            if (existing == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(existing);
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        public void Post([FromBody] Product product)
+        {
+            Context.Products.Add(product);
+            Context.SaveChanges();
+        }
+
+        // PUT: api/Products/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Product product)
+        {
+            var existing = Context.Products.AsNoTracking().FirstOrDefault(f => f.Id == id);
+            if (existing == null)
+            {
+                Context.Products.Add(product);
+                Context.SaveChanges();
+                return Ok(product);
+            }
+            product.Id = id;
+            Context.Products.Update(product);
+            Context.SaveChanges();
+            return Ok(product);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var existing = Context.Products.FirstOrDefault(product => product.Id == id);
+            if (existing == null)
+            {
+                return NotFound();
+            }
+            Context.Products.Remove(existing);
+            Context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ WebApplication1/Models/ProductDbSeeder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class ProductDbSeeder
+    {
+        public static void Initialize(ProductDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any flowers.
+            if (context.Products.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Products.AddRange(
+                new Product
+                {
+                    Name = "GPU",
+                    Description = "RTX 2080 MSI DUKE",
+                    Category = "Video processing",
+                    Price = 300
+                },
+                new Product
+                {
+                    Name = "CPU",
+                    Description = "AMD Ryzen 2700x",
+                    Category = "Processing",
+                    Price = 150
+                }
+            );
+            context.SaveChanges();
+        }
+
+    }
+}
